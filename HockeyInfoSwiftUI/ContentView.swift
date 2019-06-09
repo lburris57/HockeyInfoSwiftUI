cdf13b3f265f5a8fdd156f05719f84db81a037c0
Larry Burris <lburris@mac.com>
2019-06-09 13:12:39 -0400

    Initial Commit

---
+++ HockeyInfoSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HockeyInfoSwiftUI
+//
+//  Created by Larry Burris on 6/9/19.
+//  Copyright © 2019 Larry Burris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
