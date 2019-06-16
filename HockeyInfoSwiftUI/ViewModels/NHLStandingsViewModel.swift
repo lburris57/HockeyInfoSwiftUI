eb013347d298313eace3d64b7e76d78fa74331ea
Larry Burris <lburris@mac.com>
2019-06-16 16:57:49 -0400

    Added NHLStandingsViewModel

---
+++ HockeyInfoSwiftUI/ViewModels/NHLStandingsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NHLStandingsViewModel.swift
+//  HockeyInfoSwiftUI
+//
+//  Created by Larry Burris on 6/16/19.
+//  Copyright © 2019 Larry Burris. All rights reserved.
+//
+
+import Foundation
